5e14d2303d104c53cdc3dea5150b853104851167
Spencer Buchanan <spencerhbuchanan@protonmail.com>
2023-04-30 15:14:54 -0600

    Added comments

--- app/src/main/java/thedemo/App.java
+++ app/src/main/java/thedemo/App.java
@@ -9,11 +9,5 @@ import thedemo.Controller.MainControl;
 public class App {
     public static void main(String[] args) throws IOException {
         new MainControl();
-        // URL fileLocation = App.class.getResource("/text/test.txt");
-
-        // BufferedReader reader = new BufferedReader(new
-        // InputStreamReader(fileLocation.openStream()));
-
-        // System.out.println(reader.readLine());
     }
 }
